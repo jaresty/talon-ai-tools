ce05be234974ced4f210f18dd19c16cf20ed6a62
Joshua Aresty <jaresty@gmail.com>
2024-08-22 17:56:52 -0700

    Separate pair prompts from simple prompts
     - this helps with rule recognition

--- lib/talonSettings.py
+++ lib/talonSettings.py
@@ -9,6 +9,8 @@ mod.tag("gpt_beta", desc="Tag for enabling beta GPT commands")
 # (ie those that just take in the clipboard text)
 mod.list("staticPrompt", desc="GPT Prompts Without Dynamic Arguments")
 mod.list("customPrompt", desc="Custom user-defined GPT prompts")
+mod.list("staticPairPrompt", desc="GPT Prompts With Two Sources")
+mod.list("customPairPrompt", desc="Custom user-defined GPT prompts With Two Sources")
 mod.list("modelPrompt", desc="GPT Prompts")
 mod.list("model", desc="The name of the model")
 mod.list("modelDestination", desc="What to do after returning the model response")
@@ -33,6 +35,12 @@ def pleasePrompt(matched_prompt) -> str:
 # model prompts can be either static and predefined by this repo or custom outside of it
 @mod.capture(rule="{user.staticPrompt} | {user.customPrompt}")
 def modelSimplePrompt(matched_prompt) -> str:
+    return str(matched_prompt)
+
+
+# model prompts can be either static and predefined by this repo or custom outside of it
+@mod.capture(rule="{user.staticPairPrompt} | {user.customPairPrompt}")
+def modelSimplePairPrompt(matched_prompt) -> str:
     return str(matched_prompt)
 
 
